0c6a310d5d4292151fdbfedc8bfa02462056e90b
HafidTraikzi <hafid.traikzi@gmail.com>
2015-06-16 16:38:38 +0200

    Add fiels in form, edit array in controller option et fix footer

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -11,7 +11,7 @@ class OptionsController < ApplicationController
       params["img_header"] = filename
     end
 
-    %w[title_header text_footer link_twitter link_linkedin link_youtube id_analytics img_header bg_header color_header bg_footer color_footer mode_maintenance].each do |key|
+    %w[title_header text_footer link_twitter link_facebook link_linkedin link_google_plus id_analytics img_header bg_header color_header bg_footer color_footer mode_maintenance].each do |key|
       option = Option.where(option_key: key).first_or_initialize
       option.option_value = params[key] if params["img_header"] != filename ? params["img_header"] == filename : params[key]
       option.save if params[key].blank? != true
