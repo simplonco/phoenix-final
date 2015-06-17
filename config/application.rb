6fd8d35acb1843b237c721fde23fe8c2372584af
HafidTraikzi <hafid.traikzi@gmail.com>
2015-06-17 14:45:59 +0200

    Add config.assets.initialize_on_precompile = false in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Phoenix
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.initialize_on_precompile = false
+
     config.before_configuration do
       I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
       I18n.locale = :fr
