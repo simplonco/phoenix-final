067f0742cb5c9e8ad33cd39aa9d3767061f01384
HafidTraikzi <hafid.traikzi@gmail.com>
2015-06-19 14:40:17 +0200

    Reset

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -7,7 +7,7 @@ class OptionsController < ApplicationController
     if params["img_header"] != nil
       filename = params["img_header"].original_filename 
       new_path = Rails.root + "app/assets/images/" + filename
-      Dir.mkdir(Rails.root.join("tmp/images/").to_s) if File.directory?(Rails.root.join('tmp/images/').to_s) != true
+      Dir.mkdir(Rails.root.join("tmp").to_s) if File.directory?(Rails.root.join('tmp').to_s) != true
       FileUtils.mv(params["img_header"].tempfile.path, new_path)
       params["img_header"] = filename
     end
