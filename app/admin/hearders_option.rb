a5c9d49d0f1afac043f180cd7737393e62cb608f
HafidTraikzi <hafid.traikzi@gmail.com>
2015-07-03 14:58:21 +0200

    Add translation

--- app/admin/hearders_option.rb
+++ app/admin/hearders_option.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Headers Option" do
   menu priority: 3, label: proc{ I18n.t("active_admin.headers_option") }
   
-  content do
+  content title: proc{ I18n.t("active_admin.headers_option") }do
     render partial: 'headers_option'
   end
 end
