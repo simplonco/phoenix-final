33cec1ef90deb3942f0aaa32f2ba708771dffaa8
HafidTraikzi <hafid.traikzi@gmail.com>
2015-07-08 17:36:55 +0200

    Add multiple map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,26 +1,32 @@
 var ready;
 ready = function() {
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYmxhY2t5IiwiYSI6IjA4NWJjZDNiNDQ0MTg3YjVmZTNkM2NkMWQ3MmM4ZjU4In0.SDQh56AZPCbIL2rVs4eAkQ';
-	var map = L.mapbox.map('map', 'mapbox.streets').setView([48.855, 2.4], 8);
-	var geocoder = L.mapbox.geocoder('mapbox.places');
-	var adress = document.getElementById("map");
+	$('.map').each(function() {
+		var map = L.mapbox.map($(this).attr('id'), 'mapbox.streets').setView([48.855, 2.4], 8);
 
-	geocoder.query(adress.getAttribute("data-adress-map"), showMap);
+		var geocoder = L.mapbox.geocoder('mapbox.places');
+		var adress = document.getElementById($(this).attr('id'));
 
-	function showMap(err, data) {
-	    // The geocoder can return an area, like a city, or a
-	    // point, like an address. Here we handle both cases,
-	    // by fitting the map bounds to an area or zooming to a point.
-	    if (data.lbounds) {
-	        map.fitBounds(data.lbounds);
-	    } else if (data.latlng) {
-	        map.setView([data.latlng[0], data.latlng[1]], 13);
-	    }
-	    
-	    var marker = L.marker([data.latlng[0], data.latlng[1]]).addTo(map);
+		geocoder.query(adress.getAttribute("data-adress-map"), showMap);
 
-	    marker.bindPopup(adress.getAttribute("data-title-map") +"<br>"+ adress.getAttribute("data-adress-map"));
-	}
+		function showMap(err, data) {
+		    // The geocoder can return an area, like a city, or a
+		    // point, like an address. Here we handle both cases,
+		    // by fitting the map bounds to an area or zooming to a point.
+		    if (data.lbounds) {
+		        map.fitBounds(data.lbounds);
+		    } else if (data.latlng) {
+		        map.setView([data.latlng[0], data.latlng[1]], 13);
+		    }
+		    
+		    var marker = L.marker([data.latlng[0], data.latlng[1]]).addTo(map);
+
+		    marker.bindPopup(adress.getAttribute("data-title-map") +"<br>"+ adress.getAttribute("data-adress-map"));
+		}
+
+	})
+	//var map = L.mapbox.map('map', 'mapbox.streets').setView([48.855, 2.4], 8);
+	
 };
 
 $(document).ready(ready);
