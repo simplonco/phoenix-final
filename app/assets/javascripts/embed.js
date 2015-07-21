15418f20cc6baa266a3963d544fd181b5b74eb74
HafidTraikzi <hafid.traikzi@gmail.com>
2015-07-21 14:44:55 +0200

    Add Vimeo video

--- app/assets/javascripts/embed.js
+++ app/assets/javascripts/embed.js
@@ -20,7 +20,9 @@ ready = function() {
 
 	$('.video').each(function() {
 
-		if ("data-link-video") {
+		var embed = document.getElementById($(this).attr('id'));
+
+		if (embed.getAttribute("data-link-video") == "https://www.youtube.com/watch?v=" + getIdYoutube(embed.getAttribute("data-link-video"))) {
 
 		function getIdYoutube(url) {
 		    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
@@ -33,14 +35,13 @@ ready = function() {
 		    }
 		}
 
-		var embed = document.getElementById($(this).attr('id'));
 		var myId = getIdYoutube(embed.getAttribute("data-link-video"));
 
 		$("#" + $(this).attr('id')).html('<iframe src="//www.youtube.com/embed/' + myId + '" frameborder="0" allowfullscreen></iframe>');
 		
 		} 
 
-		else {
+		else if (embed.getAttribute("data-link-video") == getIdDailymotion(embed.getAttribute("data-link-video"))) {
 
 		function getIdDailymotion(url) {
 		    var regExp = /^.*(dailymotion.com\/|v\/|u\/\w\/|embed\/|video\?v=|\&v=)([^#\&\?]*).*/;
@@ -53,14 +54,32 @@ ready = function() {
 		    }
 		}
 
-		var embed = document.getElementById($(this).attr('id'));
 		var myId = getIdDailymotion(embed.getAttribute("data-link-video"));
 
 		$("#" + $(this).attr('id')).html('<iframe width="640" height="480" src="//www.dailymotion.com/embed/' + myId + '" frameborder="0" allowfullscreen></iframe>');
 		
 		}
+
+		else if (embed.getAttribute("data-link-video") == "https://vimeo.com/" + getIdVimeo(embed.getAttribute("data-link-video"))) {
+
+		function getIdVimeo(url) {
+		    var regExp = /^.*(vimeo.com\/|v\/|u\/\w\/|embed\/|video\?v=|\&v=)([^#\&\?]*).*/;
+		    var match = url.match(regExp);
+
+		    if (match && match[2].length == 8) {
+		        return match[2];
+		    } else {
+		        return 'error';
+		    }
+		}
+
+		var myId = getIdVimeo(embed.getAttribute("data-link-video"));
+
+		$("#" + $(this).attr('id')).html('<iframe width="640" height="480" src="//player.vimeo.com/video/' + myId + '" frameborder="0" allowfullscreen></iframe>');
+		
+		}
 	});
-};
+}
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
